4947f6e40df77d2f18ebbcd2644c17c98a9a7c91
Mariana Castro <marianacastrorc@gmail.com>
2023-04-23 21:46:17 -0300

    Fixig semantic errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,26 @@ import styles from './Footer.module.css'
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>
-        Challenge by{' '}
-        <a
-          href="https://www.frontendmentor.io/home"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by{' '}
-        <a
-          href="https://github.com/maricastroc"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mariana Castro.
-        </a>
-      </p>
+      <section>
+        <p>
+          Challenge by{' '}
+          <a
+            href="https://www.frontendmentor.io/home"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{' '}
+          <a
+            href="https://github.com/maricastroc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mariana Castro.
+          </a>
+        </p>
+      </section>
     </footer>
   )
 }
